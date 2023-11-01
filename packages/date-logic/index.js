0f0fc7e23be6ecb9191848ed8bc6dc150cd2d34f
Oleg Buyalski <zidabah2@gmail.com>
2023-11-01 20:48:21 +0300

    feat: update file

--- packages/date-logic/index.js
+++ packages/date-logic/index.js
@@ -1,4 +1,11 @@
 module.exports = function getDate(
+    locale = 'en-US',
+    options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
 ) {
-    return new Date().toDateString();
+    return new Date().toLocaleDateString(locale, options);
 };

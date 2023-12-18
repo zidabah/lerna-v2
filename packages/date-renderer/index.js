f875556480f4fa159a0827f631428129678a87d1
Oleg Buyalski <zidabah2@gmail.com>
2023-12-18 13:14:59 +0300

    feat: update package

--- packages/date-renderer/index.js
+++ packages/date-renderer/index.js
@@ -1,5 +1,5 @@
 const getDate = require('@zidabah/date-logic');
 
 (() => {
-    console.log(`@zidabah/date-renderer@0.6.0 beta 2: ${getDate()}`);
+    console.log(`@zidabah/date-renderer@0.7.0 beta 0: ${getDate()}`);
 })();
